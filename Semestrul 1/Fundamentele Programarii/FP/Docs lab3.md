e75dcd2d79eeec1162694eb5a5e9c02702eead8e
Blana-PC <top.dev.bln@gmail.com>
2024-10-11 01:27:29 +0300

    cazuri de testare , reparat cateva chestii, docs

--- Semestrul 1/Fundamentele Programarii/FP/Docs lab3.md
+++ Semestrul 1/Fundamentele Programarii/FP/Docs lab3.md
@@ -10,18 +10,21 @@
 | I4       | F4. Iesire din program                                           |
 
 ### Cazuri de testare
-#### Functia ce gaseste secv max unde oricare doua elemente consecutive difera printr-un numar prim
+
+#### Funcția ce găsește secv max unde oricare două elemente consecutive diferă printr-un număr prim
 
 | Date                                      | Rezultat               |
 | ----------------------------------------- | ---------------------- |
 | \[1,2,3,6,13,4,21,17,4,2,69,67,86,2,9,11] | \[17,4,2,69,67,86]     |
-| ----------------------------------------- | ---------------------- |
-| \[1,2,3,6,13,4,21,17,4,2,69,67,86,2,9,11] | \[17,4,2,69,67,86]     |
+| \[6,4,12,7,8,235,12,7,9,16,27]            | \[4,12,7,8]            |
+| \[19,2,3,5,7,13,17]                       | \[2,3,5,7,13,17]       |
+| \[10,12,14,15,18,23]                      | \[10,12]               |
 
-#### Functia ce gaseste secv max unde oricare doua elemente consecutive sunt de semn contrar
+#### Funcția ce găsește secv max unde oricare două elemente consecutive sunt de semn contrar
 
 | Date                         | Rezultat              |
 | ---------------------------- | --------------------- |
 | \[1,2,-3,6,-2,15,-17,3,5,-8] | \[2,-3,6,-2,15,-17,3] |
-| ---------------------------- | --------------------- |
-| \[1,2,-3,6,-2,15,-17,3,5,-8] | \[2,-3,6,-2,15,-17,3] |
+| \[-1,4,-6,8,-10,12,-14]      | \[-1,4,-6,8,-10,12]   |
+| \[0,5,-5,10,-10,15,-15]      | \[5,-5,10,-10,15,-15] |
+| \[9,-2,3,5,6]                | \[9,-2]               |
--- Semestrul 1/Fundamentele Programarii/Laboratoare/Lab3/prob1.py
+++ Semestrul 1/Fundamentele Programarii/Laboratoare/Lab3/prob1.py
@@ -1,12 +1,32 @@
-
 def test():
-    assert construire(oricare_doua_consecutive_difera_prin_nr_prim,[1,2,3,6,13,4,21,17,4,2,69,67,86,2,9,11]) == [17,4,2,69,67,86]
-    assert construire(oricare_doua_consecutive_difera_prin_semn,[1,2,-3,6,-2,15,-17,3,5,-8]) == [2,-3,6,-2,15,-17,3]
+    # Tests for oricare_doua_consecutive_difera_prin_nr_prim
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [1, 2, 3, 6, 13, 4, 21, 17, 4, 2, 69, 67, 86, 2, 9, 11]) == [17, 4, 2, 69, 67, 86]
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [5, 11, 15, 16]) == []  # fara diferente prime
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, []) == []  # lista goala
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [7]) == []  # un singur element
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [2, 5, 8, 13, 18, 23, 28]) == [2, 5, 8, 13, 18, 23, 28]  # Toata lista are proprietatea
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [10, 12, 15, 20, 25, 30]) == [10, 12, 15, 20, 25, 30]  
+    assert construire(oricare_doua_consecutive_difera_prin_nr_prim, [3, 5, 8, 10, 13, 17, 22, 27, 34]) == [3, 5, 8, 10, 13]
     
+    # Tests for oricare_doua_consecutive_difera_prin_semn
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [1, 2, -3, 6, -2, 15, -17, 3, 5, -8]) == [2, -3, 6, -2, 15, -17, 3]
+    assert construire(oricare_doua_consecutive_difera_prin_semn, []) == []  # lista goala
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [5]) == []  # un singur element
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [1, 2, 3, 4, 5]) == []  # Toate pozitive
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [-1, -2, -3, -4]) == []  # Toate negative
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [1, -1, 1, -1, 1, -1]) == [1, -1, 1, -1, 1, -1]  # Toata lista are proprietatea
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [0, -1, 1, 0, -2, 2]) == [-1, 1]  # Include 0
+    assert construire(oricare_doua_consecutive_difera_prin_semn, [10, -10, 20, -20, 30, -30, 40]) == [10, -10, 20, -20, 30, -30, 40]
+ 
+    print("All tests passed successfully!")
 
 
 
 def construire(functie,lista):
+
+    if len(lista) < 2: 
+        return []
+
     secventa = []
     temp=[]
     max=0
@@ -19,8 +39,11 @@ def construire(functie,lista):
             if temp!=[]:
                 if len(temp)>max:
                     max=len(temp)
-                    secventa=temp
+                    secventa = temp.copy()
                 temp=[]
+
+    if temp and len(temp) > max:
+      secventa = temp.copy()
 
     return secventa
 
@@ -28,6 +51,8 @@ def oricare_doua_consecutive_difera_prin_nr_prim(a,b):
     return (prim(abs(abs(a)-abs(b))))
 
 def oricare_doua_consecutive_difera_prin_semn(a,b):
+    if a==0 or b==0:
+        return False
     return (a*b<0)
 
 
@@ -59,7 +84,7 @@ def prim(n):
     if n%2==0:
         return False
     d=3
-    while(d*d<n):
+    while(d*d<=n):
         if n%d==0:
             return False
         d=d+2
