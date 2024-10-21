c65affeb6223a3e879000e2d81e96861798cf1ce
Blana-PC <top.dev.bln@gmail.com>
2024-10-21 14:17:49 +0300

    incepe refactorizare

--- Semestrul 1/Fundamentele Programarii/Laboratoare/Lab4/package.py
+++ Semestrul 1/Fundamentele Programarii/Laboratoare/Lab4/package.py
@@ -2,10 +2,25 @@ from datetime import datetime
 
 class Package:
     def __init__(self, start: datetime, end: datetime, destination: str, price: float):
+        """
+        Inițializează un obiect Package.
+
+        Args:
+            start_date (datetime): Data de început a pachetului.
+            end_date (datetime): Data de sfârșit a pachetului.
+            destination (str): Destinația pachetului.
+            price (float): Prețul pachetului.
+        """
         self.start_date = start
         self.end_date = end
         self.destination = destination
         self.price = price
 
     def __str__(self):
+        """
+        Returnează o reprezentare text a pachetului.
+
+        Returns:
+            str: Reprezentarea text a pachetului.
+        """
         return f"{self.destination} ({self.start_date.strftime('%Y-%m-%d')} - {self.end_date.strftime('%Y-%m-%d')}): {self.price} Euro"
--- Semestrul 1/Fundamentele Programarii/Laboratoare/Lab4/package_manager.py
+++ Semestrul 1/Fundamentele Programarii/Laboratoare/Lab4/package_manager.py
@@ -101,23 +101,11 @@ class PackageManager:
                 break
             except ValueError:
                 print("Pret invalid. Va rugam introduceti un numar pozitiv.")
-        
-        success = self.__add_package_internal(data[0], data[1], destination, price)
-        
-        if success:
-            print("\033[32mPachet adaugat cu succes\033[0m")
-            print(str(self.__offers[-1]))
-        else:
-            print("\033[31mEroare la adaugarea pachetului\033[0m")
-
-    def __add_package_internal(self, start_date, end_date, destination, price):
-        if not all([start_date, end_date, destination, price > 0]):
-            return False
-        
-        new_package = Package(start_date, end_date, destination, price)
+        new_package = Package(data[0], data[1], destination, price)
         self.__offers.append(new_package)
         self.__record_change('add', [new_package])
-        return True
+        print("\033[32mPachet adaugat cu succes\033[0m") 
+        print(str(new_package))
 
     def modify_package(self):
         print("\033[33mModificare pachet\033[0m")
