7a2048184cd1b0d18a16b27960869dfbcd7e3981
top.dev.bln <top.dev.bln@gmail.com>
2025-03-20 10:57:18 +0200

    lab

--- Semestrul 2/OOP/Laboratoare/Lab2/service.c
+++ Semestrul 2/OOP/Laboratoare/Lab2/service.c
@@ -9,6 +9,9 @@
 
 
 int actualizareMasinaService(Repo *repo, const char *nr_inmatriculare, const char *model_nou, const char *categorie_noua) {
+    //input : repo - pointer la Repository, nr_inmatriculare - pointer la sir de caractere, model_nou - pointer la sir de caractere, categorie_noua - pointer la sir de caractere
+    //output : returneaza 1 daca masina a fost actualizata cu succes, 0 altfel,
+    //Functia cauta masina cu numarul de inmatriculare dat si o actualizeaza cu datele introduse
     int index = cautaMasinaDupaInmatriculare(repo, nr_inmatriculare);
     if (index == -1) {
         return 0;
@@ -23,6 +26,9 @@ int actualizareMasinaService(Repo *repo, const char *nr_inmatriculare, const cha
     return 1;
 }
 int adaugaMasinaService(Repo *repo, const char *nr_inmatriculare, const char *model, const char *categorie) {
+    //input : repo - pointer la Repository, nr_inmatriculare - pointer la sir de caractere, model - pointer la sir de caractere, categorie - pointer la sir de caractere
+    //output : returneaza 1 daca masina a fost adaugata cu succes, 0 altfel
+    //Functia adauga o masina in Repository
 
     if (cautaMasinaDupaInmatriculare(repo, nr_inmatriculare) != -1) {
         return 0;
@@ -33,6 +39,9 @@ int adaugaMasinaService(Repo *repo, const char *nr_inmatriculare, const char *mo
     return adaugaMasinaRepo(repo, &m);
 }
 void inchiriereMasinaService(Repo *repo, const char *nr_inmatriculare) {
+    //input : repo - pointer la Repository, nr_inmatriculare - pointer la sir de caractere
+    //output : -
+    //Functia cauta masina cu numarul de inmatriculare dat si schimba statusul de inchiriere
     int index = cautaMasinaDupaInmatriculare(repo, nr_inmatriculare);
     if (index == -1) {
         return;
@@ -43,6 +52,9 @@ void inchiriereMasinaService(Repo *repo, const char *nr_inmatriculare) {
 }
 
 int filtrare(Repo *repo, char subOpt, const char *filtru, Masina *filtered) {
+    //input : repo - pointer la Repository, subOpt - caracter, filtru - pointer la sir de caractere, filtered - pointer la Masina
+    //output : returneaza numarul de masini filtrate
+    //Functia filtreaza masinile dupa un criteriu dat
     int count = 0;
     for (int i = 0; i < repo->dim; i++) {
         if (subOpt == '1' && strcmp(repo->masini[i].categorie, filtru) == 0) {
@@ -54,58 +66,42 @@ int filtrare(Repo *repo, char subOpt, const char *filtru, Masina *filtered) {
     return count;
 }
 
-void sortByModel(Masina *sorted, int dim) {
-    int swapped = 1;
-    while (swapped) {
-        swapped = 0;
-        for (int i = 0; i < dim - 1; i++) {
-            int cmp = strcmp(sorted[i].model, sorted[i + 1].model);
-
-            if (cmp > 0) {
-                Masina temp = sorted[i];
-                sorted[i] = sorted[i + 1];
-                sorted[i + 1] = temp;
-                swapped = 1;
-            }
-        }
-    }
-}
-void sortByCategorie(Masina *sorted, int dim) {
-    int swapped = 1;
-    while (swapped) {
-        swapped = 0;
-        for (int i = 0; i < dim - 1; i++) {
-            int cmp = strcmp(sorted[i].categorie, sorted[i + 1].categorie);
-
-            if (cmp > 0) {
-                Masina temp = sorted[i];
-                sorted[i] = sorted[i + 1];
-                sorted[i + 1] = temp;
-                swapped = 1;
-            }
-        }
-    }
-}
 
 int sortareMasini(Repo *repo, char criteriu, char ordine, Masina *sorted) {
-
+    //input : repo - pointer la Repository, criteriu - caracter, ordine - caracter, sorted - pointer la Masina
+    //output : returneaza numarul de masini sortate
+    //Functia sorteaza masinile dupa un criteriu dat , lista sortata fiind salvata in sorted
     for (int i = 0; i < repo->dim; i++) {
         sorted[i] = repo->masini[i];
     }
 
-
-    if (criteriu == '1') {
-        sortByModel(sorted, repo->dim);
-    } else if (criteriu == '2') {
-        sortByCategorie(sorted, repo->dim);
-    }
+    // Bubble sort
+    for (int i = 0; i < repo->dim - 1; i++) {
+        for (int j = 0; j < repo->dim - 1 - i; j++) {
+            int cmp = 0;
 
 
-    if (ordine == '2') {
-        for (int i = 0; i < repo->dim / 2; i++) {
-            Masina temp = sorted[i];
-            sorted[i] = sorted[repo->dim - i - 1];
-            sorted[repo->dim - i - 1] = temp;
+            if (criteriu == '1') {
+                cmp = strcmp(sorted[j].model, sorted[j + 1].model);
+            } else if (criteriu == '2') {
+                cmp = strcmp(sorted[j].categorie, sorted[j + 1].categorie);
+            }
+
+
+            if (ordine == '1') {
+                if (cmp > 0) {
+
+                    Masina temp = sorted[j];
+                    sorted[j] = sorted[j + 1];
+                    sorted[j + 1] = temp;
+                }
+            } else if (ordine == '2') {
+                if (cmp < 0) {
+                    Masina temp = sorted[j];
+                    sorted[j] = sorted[j + 1];
+                    sorted[j + 1] = temp;
+                }
+            }
         }
     }
 
