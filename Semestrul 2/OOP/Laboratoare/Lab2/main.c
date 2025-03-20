6f1cc3181f95b0855f019817f2ab3690f8466aa0
top.dev.bln <top.dev.bln@gmail.com>
2025-03-20 10:53:37 +0200

    lab

---
+++ Semestrul 2/OOP/Laboratoare/Lab2/main.c
@@ -0,0 +1,24 @@
+
+
+
+//4. Inchiriere masini
+
+//Creati o aplicatie pentru o firma de inchirieri auto. Fiecare masina are un numar de inmatriculare,
+//Aplicatia permite:
+//a) adaugare de masini
+//b) actualizare masina existenta
+//c) inchiriere masina/returnare masina
+//d) Vizualizare masini dupa un criteru dat (categorie, model)
+//e) Permite sortarea masinilor dupa: model sau categorie (crescator/descrescator)
+
+
+#include "ui.h"
+#include "tests.h"
+
+int main() {
+    run_all_tests();
+    run();
+
+
+    return 0;
+}
